762e9231def221bb141d7a7fb2a0ce0c5eb3e521
Jiheon Kim <siwmua0@naver.com>
2024-04-08 14:27:26 +0900

    apollo/client 셋업 및 graphql 설치

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import router from './routes/index.tsx';
-import worker from './_mock/worker.ts';
+import worker from './mock/worker.ts';
 import './index.css';
 
 const queryClient = new QueryClient();
@@ -11,9 +12,16 @@ if (process.env.NODE_ENV === 'development') {
   worker.start();
 }
 
+const client = new ApolloClient({
+  uri: 'https://flyby-router-demo.herokuapp.com/',
+  cache: new InMemoryCache(),
+});
+
 ReactDOM.createRoot(document.getElementById('root')!) //
   .render(
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
+      <ApolloProvider client={client}>
+        <RouterProvider router={router} />
+      </ApolloProvider>
     </QueryClientProvider>,
   );
